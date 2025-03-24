a6546f09a7b0bf91b63f43b84e3953cb8c45ae00
dqv28 <dovuong020802@gmail.com>
2025-03-24 09:08:44 +0700

    update 24/3

--- src/app/(work)/profile/(more)/schedule/page.tsx
+++ src/app/(work)/profile/(more)/schedule/page.tsx
@@ -1,8 +1,21 @@
+import { getAttendances } from '@/libs/data'
+import dayjs from 'dayjs'
 import React from 'react'
 import ScheduleHolidayCalendarCard from './components/schedule-holiday-calendar-card'
 import ScheduleHolidayCard from './components/schedule-holiday-card'
 
-const SchedulePage: React.FC = () => {
+const SchedulePage: React.FC<any> = async (prop: { searchParams: any }) => {
+  const searchParams = await prop.searchParams
+
+  const todayStr = String(dayjs(new Date()).format('YYYY-MM'))
+
+  const res = await getAttendances({
+    date: searchParams?.date || todayStr,
+    account_id: 3,
+  })
+
+  console.log('Attendances ->', res)
+
   return (
     <div className="no-scroll h-[calc(100vh-87px)] !space-y-[16px] overflow-y-auto">
       <ScheduleHolidayCard title="Thông tin ngày nghỉ" />
